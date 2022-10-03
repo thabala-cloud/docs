a3924f47d965b958a21047b9a498849d25a697c2
Peter Kosztolanyi <peter.kosztolanyi@gmail.com>
2022-10-03 03:21:55 +0200

    Add index page

---
+++ src/components/HomeCards/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+const cards = [
+  {
+    title: 'Admin Console',
+    icon: 'img/icons/admin-console.svg',
+    description: "Thabala Admin Console is the web interface to manage and administrate your services in a web browser.",
+    url: 'admin-console/introduction',
+  },
+  {
+    title: 'Thabala CLI',
+    icon: 'img/icons/cli.svg',
+    description: "Thabala Command Line Interface is the tool to manage and administrate your service programmatically.",
+    url: 'cli/overview',
+  },
+  {
+    title: 'Services',
+    icon: 'img/icons/services.svg',
+    description: "Documentation of the supported fully managed services.",
+    url: 'services/summary',
+  }
+];
+
+function FeatureItem({url, text}){
+  return (
+    <li><a className={styles.listContainerLink} href={url}>{text}</a></li>
+  );
+}
+
+function Card({title, icon, description, url }) {
+  return (
+    <article className={clsx('col col--4')}>
+      <div className={styles.card}>
+        <div className={styles.cardHeader}>
+          <img src={icon} className={styles.cardIcon}></img>
+        </div>
+        <h2>{title}</h2>
+        <div className={styles.cardContainer}>
+          <p>{description}</p>
+        </div>
+        <div class={styles.cardButton}>
+          <a href={url} class="button">Get Started</a>
+        </div>      
+      </div>
+    </article>
+  );
+}
+
+export default function HomepageFeatures() {
+    return (
+      <div className={styles.quickLinks}>
+        <h2 className={styles.cardsTitle}>Quick links</h2>
+        <section className={styles.cards}>
+            <ul className={styles.grid3col}>
+              {cards.map((props, idx) => (
+                <Card key={idx} {...props} />
+              ))}
+            </ul>
+        </section>
+      </div>
+    );
+  }
---
+++ src/pages/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import HomeCards from '@site/src/components/HomeCards';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx(styles.heroBanner)}>
+    <div className="container">
+      <img className={styles.homeLogo} src="img/thabala-long-blue.svg" />
+      <h1 className={styles.heroTitle}>Thabala Documentation</h1>
+    </div>
+  </header>
+  )
+}
+
+export default function Home() {
+  return (
+    <Layout title="Hello" description="Hello React Page">
+        <HomepageHeader />
+        <main>
+          <HomeCards />
+        </main>
+    </Layout>
+  );
+}
